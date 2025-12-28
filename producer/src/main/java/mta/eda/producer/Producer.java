cee8cb94b0678fea57ade0907fb3083fc68847d0
Yonatan Csasznik <yonacs@gmail.com>
2025-12-28 10:18:43 +0200

    Initial commit

---
+++ producer/src/main/java/mta/eda/producer/Producer.java
@@ -0,0 +1,13 @@
+package mta.eda.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Producer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Producer.class, args);
+    }
+
+}
